c849ed4b8b1f56f8e89797f2a24409b73789699e
thanhnhuong <thanhnhuong0702@gmail.com>
2024-03-25 14:09:40 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import ContactBook from '@/views/ContactBook.vue';
+
 const routes = [
     {
         path: '/',
         name: 'contactbook',
         component: ContactBook,
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'notfound',
+        component: () => import('@/views/NotFound.vue'),
     },
 ];
 const router = createRouter({
