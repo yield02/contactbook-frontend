ac55310355dca8631283580e88a79b73c1c05c81
thanhnhuong <thanhnhuong0702@gmail.com>
2024-03-25 13:59:37 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
+import router from './router';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
