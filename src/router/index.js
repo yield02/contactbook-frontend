659ff5faffcc8b39ac42999f266d2be775eebe83
thanhnhuong <thanhnhuong0702@gmail.com>
2024-03-27 14:20:41 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
         component: () => import('@/views/ContactEdit.vue'),
         props: true,
     },
+    {
+        path: '/contacts',
+        name: 'contact.add',
+        component: () => import('@/views/ContactAdd.vue'),
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
